b4da5f332f2cb2a420dbc905c6885294b033e0d7
Haise <89542501+Ninematsuno@users.noreply.github.com>
2022-04-24 19:55:01 +0530

    Update __main__.py

--- spam/__main__.py
+++ spam/__main__.py
@@ -1,5 +1,5 @@
-from PyroBot import bot
-from PyroBot.modules import start, vanitas, sibyl, spam
+from pyrogram import bot
+from pyrogram.modules import start, vanitas, sibyl, spam
 from pyrogram import filters
 
 if __name__ == "__main__":
